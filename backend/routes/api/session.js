c590e6c9091fa1dd4b0d7059461135d489b8f9c1
NoahNim <nmedoff123@gmail.com>
2021-07-15 10:38:34 -0700

    tested handle validation

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -5,8 +5,24 @@ const asyncHandler = require('express-async-handler');
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
+const validateLogin = [
+    check('credential')
+        .exists({ checkFalsy: true })
+        .notEmpty()
+        .withMessage('Please provide a valid email or username.'),
+    check('password')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a password.'),
+    handleValidationErrors,
+];
+
+//Login
 router.post(
     '/',
+    validateLogin,
     asyncHandler(async (req, res, next) => {
         const { credential, password } = req.body;
 
@@ -27,7 +43,7 @@ router.post(
         });
     }),
 );
-
+//Logout
 router.delete(
     '/',
     (_req, res) => {
@@ -35,7 +51,6 @@ router.delete(
         return res.json({ message: 'success' });
     }
 );
-
 // Restore session user
 router.get(
     '/',
