2ba2db2d0bd948d2679e9d902194e790377291da
NoahNim <nmedoff123@gmail.com>
2021-07-15 10:14:28 -0700

    tested user login authorization

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const router = require('express').Router();
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 
