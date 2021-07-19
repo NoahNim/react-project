8caaedee13a495be5bf666f87ef57d22d150c982
NoahNim <nmedoff123@gmail.com>
2021-07-19 10:22:12 -0700

    make router api path for albums

---
+++ backend/routes/api/album.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router();
+const asyncHandler = require('express-async-handler');
+
+//List Albums
+router.get('/', asyncHandler(async (req, res) => {
+    const albums = await db.Album.findAll();
+
+    return res.json({albums})
+}));
