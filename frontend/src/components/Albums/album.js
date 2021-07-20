abe47ce54f87307309727afc1937fa5d1c766054
NoahNim <nmedoff123@gmail.com>
2021-07-19 23:05:39 -0700

    album pages now show the correct photo

--- frontend/src/components/Albums/album.js
+++ frontend/src/components/Albums/album.js
@@ -9,27 +9,30 @@ function Album() {
     const photos = useSelector(state => state.albums);
     const sessionUser = useSelector(state => state.session.user);
     const photoArr = Object.values(photos);
-    console.log(photoArr);
     const dispatch = useDispatch();
 
     useEffect(() => {
         dispatch(getAlbum(id));
     }, [dispatch, id]);
 
-    return (
-        <div>
-            {photoArr.map(photo => {
-                if (sessionUser.id === photo.userId) {
-                    return (
-                        <div>
-                            <h2 key={photo.name}>{photo.name}</h2>
-                            <img key={photo.name} src={photo.imgUrl} alt="meow" height="100" width="100"></img>
-                        </div>
-                    )
-                }
-            })}
-        </div>
-    )
+    if (sessionUser) {
+        return (
+            <div>
+                {photoArr.map(photo => {
+                    console.log(photo.albumId)
+                    if (sessionUser.id === photo.userId && photo.albumId === Number(id)) {
+                        return (
+                            <div>
+                                <h2 key={photo.name}>{photo.name}</h2>
+                                <img key={photo.id} src={photo.imgUrl} alt="meow" height="100" width="100"></img>
+                            </div>
+                        )
+                    }
+                })}
+            </div>
+        )
+    }
+
 }
 
 export default Album;
